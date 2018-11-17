fabc8a0d24427da08070abbdca7072e26d8def04
MartinR01 <martin.ryba01@gmail.com>
2018-11-17 19:03:42 +0100

    added multiple page navigation

--- index.php
+++ index.php
@@ -4,10 +4,27 @@
   $loader = new Twig_Loader_Filesystem('templates');
   $twig = new Twig_Environment($loader); // no cache
 
+  $data = array();
+  
+  if(isset($_GET['page'])){
+    switch ($_GET['page']) {
+      case 'movie':
+        $sablona='movie.twig';
+        $data['title'] = 'Revenant';
+        $data['poster'] = 'poster.jpeg';
+        $data['info'] = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+        $data['actors'] = 'Jakie Chan';
+        $data['comments'] = 'Love this movie';
+        break;
 
-  $sablona='movie.twig';
+      default:
+        $sablona = "home.twig";
+        break;
+    }
+  }else{
+    $sablona = "home.twig";
+  }
+
   $template = $twig->loadTemplate($sablona);
-  $data['title'] = 'Revenant';
-  ///// vypsani vysledku
   echo $template->render($data);
 ?>
