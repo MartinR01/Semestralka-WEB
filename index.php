e0dd471447939e2e366a1b506250762d37ae5b7d
MartinR01 <martin.ryba01@gmail.com>
2018-11-17 22:25:54 +0100

    db connection working

--- index.php
+++ index.php
@@ -5,7 +5,31 @@
   $twig = new Twig_Environment($loader); // no cache
 
   $data = array();
-  
+
+  //DB variables
+  $host="localhost";
+  $dbname="semestralka_db1";
+  $username="root";
+  $password="";
+
+  $dns = "mysql:host=$host;dbname=$dbname";
+
+
+
+  try {
+  	$db = new PDO($dns,$username,$password);
+  	$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    $dotaz = $db->prepare('SELECT * FROM test');
+    $dotaz->execute();
+    $result = $dotaz->fetch(PDO::FETCH_ASSOC); // jinak to duplikuje data
+
+    print_r($result);
+
+  } catch(PDOException $e){
+  	echo "Error: " . $e->getMessage();
+  }
+
 
   if(isset($_GET['page'])){
     switch ($_GET['page']) {
@@ -15,7 +39,7 @@
         $data['poster'] = 'poster.jpeg';
         $data['info'] = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
         $data['actors'] = 'Jakie Chan';
-        $data['comments'] = 'Love this movie';
+        $data['comments'] = $result['text'];
         break;
 
       default:
