25e7c1b68ba769ca7ced2e74ccd9771cfb46d3e8
MartinR01 <martin.ryba01@gmail.com>
2018-11-18 20:34:19 +0100

    added actor page

--- index.php
+++ index.php
@@ -17,24 +17,25 @@
 
 
 
-  try {
-  	$db = new PDO($dns,$username,$password);
-  	$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-    $dotaz = $db->prepare('SELECT * FROM film WHERE idFilm=:id');
-    $dotaz->bindParam(':id',$id);
-    $id=$_GET['id'];
-    $dotaz->execute();
-    $result = $dotaz->fetch(PDO::FETCH_ASSOC); // jinak to duplikuje data
-
-  } catch(PDOException $e){
-  	echo "Error: " . $e->getMessage();
-  }
 
 
   if(isset($_GET['page'])){
     switch ($_GET['page']) {
       case 'movie':
+        try {
+          $db = new PDO($dns,$username,$password);
+          $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+          $dotaz = $db->prepare('SELECT * FROM film WHERE idFilm=:id');
+          $dotaz->bindParam(':id',$id);
+          $id=$_GET['id'];
+          $dotaz->execute();
+          $result = $dotaz->fetch(PDO::FETCH_ASSOC); // jinak to duplikuje data
+
+        } catch(PDOException $e){
+          echo "Error: " . $e->getMessage();
+        }
         $sablona='movie.twig';
         $data['title'] = $result['nazev'];
         $data['poster'] = 'images/movie_posters/'.$result['plakat_url'];
@@ -42,6 +43,27 @@
         $data['movieID'] = $id;
         $data['actors'] = 'Placeholder actor';
         $data['comments'] = 'Placeholder comment';
+        break;
+
+      case 'actor':
+        try {
+          $db = new PDO($dns,$username,$password);
+          $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+          $dotaz = $db->prepare('SELECT * FROM herec WHERE idHerec=:id');
+          $dotaz->bindParam(':id',$id);
+          $id=$_GET['id'];
+          $dotaz->execute();
+          $result = $dotaz->fetch(PDO::FETCH_ASSOC); // jinak to duplikuje data
+
+          $sablona='actor.twig';
+          $data['name'] = $result['jmeno'];
+          $data['bio'] = $result['bio'];
+          $data['photo'] = 'images/actors/'.$result['foto_url'];
+
+        } catch(PDOException $e){
+          echo "Error: " . $e->getMessage();
+        }
         break;
 
       default:
