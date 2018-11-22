cb388e192296e79c77dbdd618700021a59a89d46
MartinR01 <martin.ryba01@gmail.com>
2018-11-22 12:49:27 +0100

    now you can add comment from webpage.

--- ajax.php
+++ ajax.php
@@ -12,8 +12,14 @@ function comment(){
     $db->query(Query::movieAddComment, array('movieID' => $_POST['movieID'], 'userID' => $_POST['userID'], 'text' => $_POST['text']));
 }
 
+function rate(){
+    $db = DB::getInstance();
+    $db->query(Query::movieAddRating, array('movieID' => $_POST['movieID'], 'userID' => $_POST['userID'], 'text' => $_POST['text'], 'rating' => $_POST['rating']));
+}
+
 // choose correct action
 switch ($_POST['action']){
     case 'favorite': favorite(); break;
     case 'comment': comment(); break;
+    case 'rate': rate(); break;
 }
--- js/movie.js
+++ js/movie.js
@@ -1,11 +1,13 @@
+let params = new URLSearchParams(window.location.search);
+let movieID = params.get('id');
 let userID = 1;
 
-function toggleFav(movieID) {
+function toggleFav() {
     let btn = $("#favBtn");
     btn.toggleClass("fas");
     btn.toggleClass("far");
 
-    $.post("favorite.php",
+    $.post("ajax.php",
     {
         action: 'favorite',
         userID: userID,
@@ -15,17 +17,35 @@ function toggleFav(movieID) {
     });
 }
 
-function comment(movieID) {
+function comment() {
     let text = $('#comment').val();
     if(text.length == 0){
         alert('empty comment!');
         return;
     }
-    $.post("favorite.php",
+    $.post("ajax.php",
         {
             action: 'comment',
             userID: userID,
             movieID: movieID,
+            text: text
+        },
+        function(data, status) {
+            location.reload();
+        }
+    );
+}
+
+function rate() {
+    let text=$('#ratingText').val();
+    let rating = $('#rating').val();
+
+    $.post("ajax.php",
+        {
+            action: 'rate',
+            userID: userID,
+            movieID: movieID,
+            rating: rating,
             text: text
         },
         function(data, status) {
